b3f7c0460dc374030f551750287d3fdfeba03e36
azibismail <2020602048@student.uitm.edu.my>
2022-06-29 18:33:15 +0800

    Lab 6 client

---
+++ Lab6owncode.py
@@ -0,0 +1,27 @@
+import socket
+import signal
+import sys
+
+ClientSocket = socket.socket()
+host = '10.1.1.85'
+port = 8888
+
+print('Waiting for connection from server')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+Response = ClientSocket.recv(1024)
+print(Response.decode("utf-8"))
+while True:
+    Input = input('Enter input: ')
+
+    if Input == 'exit':
+        break
+    else:
+        ClientSocket.send(str.encode(Input))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode("utf-8"))
+
+ClientSocket.close()
